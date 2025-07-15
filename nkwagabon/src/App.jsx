e6bfa6f11af33f3dcb6e2eb664be30955783af74
ORIANEESTHER <orianenganga14@gmail.com>
2025-07-16 00:27:41 +0200

    modifFront

--- nkwagabon/src/App.jsx
+++ nkwagabon/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar"; // ← AJOUTER
 import Accueil from "./pages/Accueil";
 import Restaurants from "./pages/Restaurants";
 import Evenements from "./pages/Evenements";
@@ -8,17 +9,16 @@ import Compte from "./pages/Compte";
 import Admin from "./pages/Admin";
 import Places from "./pages/Places";
 
-
-
 function App() {
   return (
     <BrowserRouter>
+      <Navbar /> {/* Toujours affichée */}
       <Routes>
         <Route path="/" element={<Accueil />} />
         <Route path="/restaurants" element={<Restaurants />} />
-        <Route path="/events" element={<Evenements />} />
+        <Route path="/evenements" element={<Evenements />} />
         <Route path="/login" element={<Connexions />} />
-        <Route path="/register" element={<Inscription />} />
+        <Route path="/sign" element={<Inscription />} />
         <Route path="/compte" element={<Compte />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/places" element={<Places />} />
--- nkwagabon/src/components/Navbar.jsx
+++ nkwagabon/src/components/Navbar.jsx
@@ -4,15 +4,23 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-success px-3">
-            <Link className="navbar-brand fw-bold" to="/">Nkwagabon</Link>
-            <div className="collapse navbar-collapse">
-                <ul className="navbar-nav ms-auto">
-                    <li className="nav-item"><Link to="/" className="nav-link">Accueil</Link></li>
-                    <li className="nav-item"><Link to="/restaurants" className="nav-link">Restaurants</Link></li>
-                    <li className="nav-item"><Link to="/evenements" className="nav-link">Événements</Link></li>
-                    <li className="nav-item"><Link to="/compte" className="nav-link">Compte</Link></li>
-                    <li className="nav-item"><Link to="/admin" className="nav-link">Admin</Link></li>
-                </ul>
+            <div className="container-fluid">
+                <Link className="navbar-brand fw-bold" to="/">Nkwagabon</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item"><Link to="/" className="nav-link">Accueil</Link></li>
+                        <li className="nav-item"><Link to="/restaurants" className="nav-link">Restaurants</Link></li>
+                        <li className="nav-item"><Link to="/evenements" className="nav-link">Événements</Link></li>
+                        <li className="nav-item"><Link to="/places" className="nav-link">Lieux</Link></li>
+                        <li className="nav-item"><Link to="/compte" className="nav-link">Compte</Link></li>
+                        <li className="nav-item"><Link to="/login" className="nav-link">Connexion</Link></li>
+                        <li className="nav-item"><Link to="/sign" className="nav-link">Inscription</Link></li>
+                        <li className="nav-item"><Link to="/admin" className="nav-link">Admin</Link></li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
